7832cf5f8b01531904ffd778457cda1f84d09c61
Al-amintpi <alamincmt7418@gmail.com>
2020-10-14 01:29:18 +0600

    compete single page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,4 +6,37 @@ $(document).ready(function(){
 	 $('.test-popup').magnificPopup({
 	 	type:'image'
 	 });
+
+	 $('.menu ul li a').click(function(){
+	 	var value = $(this).attr('href');
+	 	 
+	 	$('html, body').animate({
+	 		scrollTop:$(value).offset().top
+	 	}, 1000);
+	 	return false;
+	 });
+
+	 $('.uptoscrollbtn').click(function(){
+	 	$('html, body').animate({
+	 		scrollTop:0
+	 	}, 1000);
+	 	return false;
+	 });
+
+});
+
+$(window).scroll(function(){
+	 	var scrollvalue = $(this).scrollTop();
+	 	if(scrollvalue>400){
+	 		$('.uptoscrollbtn').show();
+	 	}else{
+	 		$('.uptoscrollbtn').hide();
+	 	}
+
+	 	if(scrollvalue>200){
+	 		$('.header-top-area').addClass('fixedmenu');
+	 	}else{
+	 		$('.header-top-area').removeClass('fixedmenu');
+	 	}
+
 });
